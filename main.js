249b77160fc6c1d2f3b4347d5c7e3189d18fa788
Trisha-Nohara <91459748+Trisha-Nohara@users.noreply.github.com>
2023-07-26 19:46:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function setup(){
+    canvas= createCanvas(550,550);
+    canvas.position(560,100);
+    video=createCapture(VIDEO);
+    video.size(550,500);
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);    
+}
+
+function modelLoaded(){
+    console.log("PoseNet is initialized");
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+    }
+}
+
+function draw(){
+    background('#969A97');
+}
